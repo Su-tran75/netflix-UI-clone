f4d516f7db074c54b04dff53faa0cc9f41bf4841
su-tran75 <su-tran@hotmail.fr>
2021-10-25 16:05:53 +0800

    connect mongoDB and protect info with .env file

--- api/index.js
+++ api/index.js
@@ -1,6 +1,15 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => console.log("DB connection successfull !"))
+  .catch((err) => console.log(err));
 
 app.listen(8800, () => {
-  console.log("backend server is running");
+  console.log("backend server is running !");
 });
