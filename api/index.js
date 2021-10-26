7b8c93ebf786c89b75c4b0c87db6409d76d32efd
su-tran75 <su-tran@hotmail.fr>
2021-10-26 10:47:09 +0800

    create API routes and connect user registration with mongoDB

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+const authRoute = require("./routes/auth");
 
 dotenv.config();
 
@@ -9,6 +10,10 @@ mongoose
   .connect(process.env.MONGO_URL)
   .then(() => console.log("DB connection successfull !"))
   .catch((err) => console.log(err));
+
+app.use(express.json());
+
+app.use("/api/auth", authRoute);
 
 app.listen(8800, () => {
   console.log("backend server is running !");
