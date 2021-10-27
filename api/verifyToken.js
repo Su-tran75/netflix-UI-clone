f2d3b525d393f0ddccee74fa5279cd8fdafaa739
su-tran75 <su-tran@hotmail.fr>
2021-10-27 14:42:29 +0800

    create delete, get and getAll users function and connect to mongobd

--- api/verifyToken.js
+++ api/verifyToken.js
@@ -4,7 +4,7 @@ function verify(req, res, next) {
   const authHeader = req.headers.token;
   if (authHeader) {
     const token = authHeader.split(" ")[1];
-    console.log("verify -> token", token);
+    //console.log("verify -> token", token);
 
     jwt.verify(token, process.env.SECRET_KEY, (err, user) => {
       if (err) res.status(403).json("Token in not valid !");
