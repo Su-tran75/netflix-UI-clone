9931794afbe3dd0ca1043a2e92f0ced86476cb17
su-tran75 <su-tran@hotmail.fr>
2021-11-04 20:22:45 +0800

    create login logic

--- admin/src/pages/login/Login.jsx
+++ admin/src/pages/login/Login.jsx
@@ -1,13 +1,41 @@
 import "./login.scss";
-import React from "react";
+import React, { useContext } from "react";
+import { useState } from "react";
+import { AuthContext } from "../../context/authContext/AuthContext";
+import { login } from "../../context/authContext/apiCalls";
 
 export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const { isFetching, dispatch } = useContext(AuthContext);
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    login({ email, password }, dispatch);
+  };
+
   return (
     <div className="login">
       <form className="loginForm">
-        <input type="text" className="loginInput" placeholder="email" />
-        <input type="password" className="loginInput" placeholder="password" />
-        <button className="loginButton">Login</button>
+        <input
+          type="text"
+          className="loginInput"
+          placeholder="email"
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type="password"
+          className="loginInput"
+          placeholder="password"
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button
+          className="loginButton"
+          onClick={handleLogin}
+          disabled={isFetching}
+        >
+          Login
+        </button>
       </form>
     </div>
   );
