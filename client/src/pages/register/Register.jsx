6a99e64f0c6b79b38f95dbab0b626df7a1b2314d
su-tran75 <su-tran@hotmail.fr>
2021-11-04 11:07:00 +0800

    test

--- client/src/pages/register/Register.jsx
+++ client/src/pages/register/Register.jsx
@@ -32,7 +32,7 @@ const Register = () => {
         <h1>Unlimited movies, TV shows, and more.</h1>
         <h2>Watch anywhere. Cancel anytime</h2>
         <p>
-          Ready to watch? Enter your email to create or restart your membership.
+          Ready to watch ? Enter your email to create or restart your membership.
         </p>
         {!email ? (
           <div className="input">
