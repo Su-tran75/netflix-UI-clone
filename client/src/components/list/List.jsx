438d35fedb4867e0dd81a3fdfe8aad4cf4e7c5b6
su-tran75 <su-tran@hotmail.fr>
2021-11-12 17:10:29 +0800

    handle carousel limit

--- client/src/components/list/List.jsx
+++ client/src/components/list/List.jsx
@@ -10,6 +10,7 @@ import { useState } from "react";
 const List = ({ list }) => {
   const [slideNumber, setSlideNumber] = useState(0);
   const [isMoved, setIsMoved] = useState(false);
+  const [clickLimit, setClickLimit] = useState(window.innerWidth / 230);
 
   const listRef = useRef();
 
@@ -20,7 +21,7 @@ const List = ({ list }) => {
       setSlideNumber(slideNumber - 1);
       listRef.current.style.transform = `translateX(${230 + distance}px)`;
     }
-    if (direction === "right" && slideNumber < 5) {
+    if (direction === "right" && slideNumber < 10 - clickLimit) {
       setSlideNumber(slideNumber + 1);
       listRef.current.style.transform = `translateX(${-230 + distance}px)`;
     }
